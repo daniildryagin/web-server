dde24af89e53073ad034a68701239053df303cf9
daniildryagin <daniildryagin.11@gmail.com>
2024-12-08 14:06:19 +0300

    Обновил миграции

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './posts/entities/post.entity';
 import { AuthModule } from './auth/auth.module';
 import { TypeormConfig } from './config/typeorm.config';
 import { DataSourceOptions } from 'typeorm';
--- src/database/migrations/1733655499692-user-is-admin.ts
+++ src/database/migrations/1733655499692-user-is-admin.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class UserIsAdmin1733651683717 implements MigrationInterface {
-    name = 'UserIsAdmin1733651683717'
+export class UserIsAdmin1733655499692 implements MigrationInterface {
+    name = 'UserIsAdmin1733655499692'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "user" ADD "isAdmin" boolean NOT NULL DEFAULT false`);
