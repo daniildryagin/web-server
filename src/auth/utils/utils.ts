8b56a6f40110a59e042d814bece049dab8c37af9
daniildryagin <daniildryagin.11@gmail.com>
2024-12-09 18:25:12 +0300

    Добавил метод для прикрепления user data в request

--- src/auth/utils/utils.ts
+++ src/auth/utils/utils.ts
@@ -1,10 +1,17 @@
 import { Request } from "express";
+import { UserDataRequest } from "../types/user-data-request.type";
 
 export const utils = {
 
   extractToken(req: Request): string {
     const [type, accessToken] = req.headers.authorization?.split(' ') || [];
-    return type.toLowerCase() === 'bearer' ? accessToken : undefined;
+    return type?.toLowerCase() === 'bearer' ? accessToken : undefined;
   },
 
+  attachUserDataToRequest(request: Request, user: UserDataRequest): Request {
+
+    request['user'] = { ...user };
+
+    return request;
+  }
 }
