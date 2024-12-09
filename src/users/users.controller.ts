e6a95b5eb881823083ca9f38673689b92d241873
daniildryagin <daniildryagin.11@gmail.com>
2024-12-09 18:27:54 +0300

    Изменения users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PostsService } from 'src/posts/posts.service';
-import { User } from './entities/user.entity';
-import { DeleteResult, UpdateResult } from 'typeorm';
+import { UpdateResult } from 'typeorm';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { UserDto } from './dto/user.dto';
+import { UsersGuard } from './guards/users.guard';
+import { PostDto } from 'src/posts/dto/post.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AdminGuard } from 'src/common/guards/admin.guard';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(
@@ -13,33 +19,48 @@ export class UsersController {
     private readonly postsService: PostsService
   ) { }
 
-  @UseGuards(AuthGuard)
   @Get()
-  async getAll(): Promise<User[]> {
+  async getAll(): Promise<UserDto[]> {
     return await this.usersService.getAllUsers();
   }
 
-  @UseGuards(AuthGuard)
   @Get(':id')
-  async getUserById(@Param('id', ParseIntPipe) id: number): Promise<User> {
-    return await this.usersService.getUserById(id);
+  async getUserById(@Param('id', ParseIntPipe) id: number): Promise<UserDto> {
+    return await this.usersService.findOneById(id);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminGuard)
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
+    return await this.usersService.createNewUser(createUserDto);
+  }
+
+  @UseGuards(UsersGuard)
   @Patch(':id')
-  async updateUserById(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto): Promise<UpdateResult> {
+  async updateUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto
+  ): Promise<UpdateResult> {
     return await this.usersService.updateUserById(id, updateUserDto);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(UsersGuard)
   @Delete(':id')
-  async deleteUserById(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+  async deleteUserById(@Param('id', ParseIntPipe) id: number): Promise<UserDto> {
     return await this.usersService.deleteUserById(id);
   }
 
-  @UseGuards(AuthGuard)
   @Get(':id/posts')
-  getPostsByAuthor(@Param('id', ParseIntPipe) userId: number) {
-    return this.postsService.findAllByAuthor(userId);
+  async getPostsByAuthor(@Param('id', ParseIntPipe) userId: number): Promise<PostDto[]> {
+    return await this.postsService.findAllByAuthor(userId);
+  }
+
+  @UseGuards(UsersGuard)
+  @Post(':id/change-password')
+  async changePassword(
+    @Param('id', ParseIntPipe) userId: number,
+    @Body() changePasswordDto: ChangePasswordDto
+  ): Promise<UpdateResult> {
+    return await this.usersService.changePassword(userId, changePasswordDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
-import { UserNoPassword } from './types/UserNoPassword';
+import { UserDto } from './dto/user.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 
 @Injectable()
@@ -17,43 +18,58 @@ export class UsersService {
   ) { }
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
+
     const { email, password } = createUserDto;
+
     const user = await this.usersRepository.findOneBy({ email });
 
     if (user) {
-      throw new BadRequestException('Этот email уже занят.')
+      throw new BadRequestException('Этот email уже занят.');
     }
 
-    const encryptedPassword = await bcrypt.hash(password, 10);
+    const encryptedPassword = await this.hashPassword(password);
 
-    const newUser = this.usersRepository.create({
-      ...createUserDto,
-      password: encryptedPassword
-    })
+    const newUser = this.usersRepository.create(
+      {
+        ...createUserDto,
+        password: encryptedPassword
+      }
+    );
 
     return newUser;
   }
 
-  async saveUser(user: User): Promise<UserNoPassword> {
-    const { password, ...savedUser } = await this.usersRepository.save(user);
+  async createNewUser(createUserDto: CreateUserDto): Promise<UserDto> {
+    const user = await this.createUser(createUserDto);
 
-    return savedUser;
+    return this.transformUser(await this.saveUser(user));
   }
 
-  async getAllUsers(): Promise<User[]> {
-    return await this.usersRepository.find();
+  async saveUser(user: User): Promise<User> {
+    return await this.usersRepository.save(user);
   }
 
+  async getAllUsers(): Promise<UserDto[]> {
+    const users = await this.usersRepository.find();
+
+    return users.map(user => this.transformUser(user));
+  }
 
   async getUserById(id: number): Promise<User> {
 
     const user = await this.usersRepository.findOneBy({ id });
 
     if (!user) {
-      throw new BadRequestException(`Пользователя с id = ${id} не существует`);
+      throw new BadRequestException(`Пользователь с id=${id} не найден`);
     }
 
     return user;
+  }
+
+  async findOneById(id: number): Promise<UserDto> {
+    const user = await this.getUserById(id);
+
+    return this.transformUser(user);
   }
 
   async getUserByEmail(email: string): Promise<User> {
@@ -67,26 +83,46 @@ export class UsersService {
     return user;
   }
 
+  async findUserByEmail(email: string): Promise<UserDto> {
+    const user = await this.getUserByEmail(email);
 
-  async updateUserById(id: number, updateUserDto: UpdateUserDto): Promise<UpdateResult> {
-
-    const user = await this.usersRepository.findOneBy({ id });
-
-    if (!user) {
-      throw new BadRequestException(`Пользователя с id = ${id} не существует`);
-    }
-
-    return await this.usersRepository.update({ id }, { ...updateUserDto });
+    return this.transformUser(user);
   }
 
-  async deleteUserById(id: number): Promise<DeleteResult> {
 
-    const user = await this.usersRepository.findOneBy({ id });
+  async updateUserById(id: number, updateUserDto: UpdateUserDto): Promise<UpdateResult> {
+    const user = await this.getUserById(id);
 
-    if (!user) {
-      throw new BadRequestException(`Пользователя с id = ${id} не существует`);
-    }
+    return await this.usersRepository.update({ id: user.id }, { ...updateUserDto });
+  }
 
-    return await this.usersRepository.delete({ id });
+  async deleteUserById(id: number): Promise<UserDto> {
+
+    const user = await this.getUserById(id);
+
+    const removedUser = await this.usersRepository.remove(user);
+
+    return this.transformUser(removedUser);
+  }
+
+  async changePassword(userId: number, changePasswordDto: ChangePasswordDto): Promise<UpdateResult> {
+
+    const { password } = changePasswordDto;
+
+    const user = await this.getUserById(userId);
+
+    const hashedPassword = await this.hashPassword(password);
+
+    return this.usersRepository.update({ id: user.id }, { password: hashedPassword })
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 10);
+  }
+
+  transformUser(user: User): UserDto {
+    const { password, ...transformedUser } = user;
+
+    return transformedUser;
   }
 }
