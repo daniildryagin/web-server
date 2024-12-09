531c9cb7ce3e8392d0d0cbb8847087cebe2786ec
daniildryagin <daniildryagin.11@gmail.com>
2024-12-09 18:27:39 +0300

    Добавил UserGuard

---
+++ src/users/guards/users.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { UsersService } from "src/users/users.service";
+
+@Injectable()
+export class UsersGuard implements CanActivate {
+
+  constructor(
+    private readonly usersService: UsersService,
+  ) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+
+    const req = context.switchToHttp().getRequest();
+
+    const userIdToAccess = Number(req.params.id);
+
+    const user = await this.usersService.getUserById(req['user'].id);
+
+    if (!user.isAdmin && user.id !== userIdToAccess) {
+      return false;
+    }
+
+    return true;
+  }
+}
