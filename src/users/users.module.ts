94ba61902e7f88566563af5394a5a56e9cf255c3
daniildryagin <daniildryagin.11@gmail.com>
2024-12-12 17:45:11 +0300

    Убрал импорт

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,13 +5,12 @@ import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from 'src/posts/entities/post.entity';
 import { PostsService } from 'src/posts/posts.service';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [UsersService, PostsService],
   controllers: [UsersController],
   imports: [
-    TypeOrmModule.forFeature([User, Post])
+    TypeOrmModule.forFeature([User, Post]),
   ],
   exports: [UsersService]
 })
